aea48f1d0d713e7eb5ff79f7b3d5b7e137ec4a0c
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2021-06-13 16:17:12 +0200

    Message queue.

---
+++ server/queue.c
@@ -0,0 +1,92 @@
+
+// C code to implement Priority Queue
+// using Linked List
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node
+typedef struct node {
+  char* message;
+  char* user_name;
+
+  // Lower values indicate higher priority
+  int priority;
+
+  struct node* next;
+
+} Node;
+
+// Function to Create A New Node
+Node* newNode(char* message, char* user_name, int priority) {
+  Node* temp = malloc(sizeof(Node));
+  temp->message = message;
+  temp->user_name = user_name;
+  temp->priority = priority;
+  temp->next = NULL;
+
+  return temp;
+}
+
+// Return the value at head
+int peek(Node** head) { return (*head)->message; }
+
+// Removes the element with the
+// highest priority form the list
+void pop(Node** head) {
+  Node* temp = *head;
+  (*head) = (*head)->next;
+  free(temp);
+}
+
+// Function to push according to priority
+void push(Node** head, char* message, char* user_name, int priority) {
+  Node* start = (*head);
+
+  // Create new Node
+  Node* temp = newNode(message, user_name, priority);
+
+  if (isEmpty(head)) {
+    (*head) = temp;
+    return;
+  }
+
+  // Special Case: The head of list has lesser
+  // priority than new node. So insert new
+  // node before head node and change head node.
+  if ((*head)->priority > priority) {
+    // Insert New Node before head
+    temp->next = *head;
+    (*head) = temp;
+  } else {
+    // Traverse the list and find a
+    // position to insert new node
+    while (start->next != NULL && start->next->priority < priority) {
+      start = start->next;
+    }
+
+    // Either at the ends of the list
+    // or at required position
+    temp->next = start->next;
+    start->next = temp;
+  }
+}
+
+// Function to check is list is empty
+int isEmpty(Node** head) { return (*head) == NULL; }
+
+// Driver code
+// int main() {
+//   // Create a Priority Queue
+//   // 7->4->5->6
+//   Node* pq = newNode(4, 1);
+//   push(&pq, 5, 2);
+//   push(&pq, 6, 3);
+//   push(&pq, 7, 0);
+
+//   while (!isEmpty(&pq)) {
+//     printf("%d ", peek(&pq));
+//     pop(&pq);
+//   }
+
+//   return 0;
+// }
